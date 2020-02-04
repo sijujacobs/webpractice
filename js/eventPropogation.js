77503e95a59c3533f3a9c29acdd3d004f6a706b4
SIJU JACOB <SIJUJACOBS@GMAILCOM>
2020-02-04 18:25:59 -0500

    css changes

--- js/eventPropogation.js
+++ js/eventPropogation.js
@@ -1,4 +1,4 @@
-$(document).ready(function(){
+$(document).ready(function () {
     let capturePhases = [];
     let bubblePhases = [];
     var outeDiv = document.getElementById("outerDiv");
@@ -11,7 +11,7 @@ $(document).ready(function(){
     var btnStopProp = document.getElementById("btnStopProp");
     var btnStopImmedProp = document.getElementById("btnStopImmedProp");
     var preDefaultCheckBox = document.getElementById("preDefaultCheckBox");
-    
+
 
     btnReset.addEventListener("click", resetHandler);
 
@@ -39,72 +39,75 @@ $(document).ready(function(){
     function capturingPhaseClickHandler(event) {
         capturePhases.push(event.currentTarget.className);
         setCapturingSection();
-    } 
-    function setCapturingSection(){
+    }
+    function setCapturingSection() {
         $('div.capturingSection').empty();
+        
+        // $.each(capturePhases, function (i, cp) {
         capturePhases.forEach((cp, i) => {
-            $('div.capturingSection').append('<div>' + (i + 1) + " : " +cp + '</div>');
+            var newElement = $('<div class="phaseItem"> ' + (i + 1) + " : " + cp + ' </div>');
+            $('div.capturingSection').append(newElement);
         });
     }
 
     function bubblingPhaseClickHandler(event) {
         bubblePhases.push(event.currentTarget.className);
         setBubblingSection();
-    } 
-    function setBubblingSection(){
+    }
+    function setBubblingSection() {
         $('div.bubblingSection').empty();
         bubblePhases.forEach((cp, i) => {
-            $('div.bubblingSection').append('<div>' + (i + 1) + " : " +cp + '</div>');
+            var newElement = $('<div class="phaseItem"> ' + (i + 1) + " : " + cp + ' </div>');
+            
+            $('div.bubblingSection').append(newElement);
         });
     }
-//----------- STOP PROPOGATION ----------------------------------
-        function stopPropogationCapturingHandler(event) {
-             capturePhases.push(event.currentTarget.className);
-             setCapturingSection();
-        } 
-        function stopPropogationBubblingHandler(event) {
-             event.stopPropagation();
-             bubblePhases.push(event.currentTarget.className);
-             setBubblingSection();
-        } 
+    //----------- STOP PROPOGATION ----------------------------------
+    function stopPropogationCapturingHandler(event) {
+        capturePhases.push(event.currentTarget.className);
+        setCapturingSection();
+    }
+    function stopPropogationBubblingHandler(event) {
+        event.stopPropagation();
+        bubblePhases.push(event.currentTarget.className);
+        setBubblingSection();
+    }
 
-        function stopImmePropogationCapturingHandler(event) {
-            capturePhases.push(event.currentTarget.className);
-            setCapturingSection();
-        } 
+    function stopImmePropogationCapturingHandler(event) {
+        capturePhases.push(event.currentTarget.className);
+        setCapturingSection();
+    }
 
-        function stopImmePropogationBubblingHandler(event) {
-            event.stopImmediatePropagation();
-            bubblePhases.push(event.currentTarget.className);
-            setBubblingSection();
-        } 
+    function stopImmePropogationBubblingHandler(event) {
+        event.stopImmediatePropagation();
+        bubblePhases.push(event.currentTarget.className);
+        setBubblingSection();
+    }
 
-        function stopPropogationOtherHandler(event) {
-            bubblePhases.push(event.currentTarget.className + " - Other EventHandler");
-            setBubblingSection();
-        } 
+    function stopPropogationOtherHandler(event) {
+        bubblePhases.push(event.currentTarget.className + " - Other EventHandler");
+        setBubblingSection();
+    }
 
-        function stopImmePropogationOtherHandler(event) {
-            // console.log("stopImmePropogationOtherHandler ");
-            bubblePhases.push(event.currentTarget.className + " - Other EventHandler");
-            setBubblingSection();
-        } 
+    function stopImmePropogationOtherHandler(event) {
+        bubblePhases.push(event.currentTarget.className + " - Other EventHandler");
+        setBubblingSection();
+    }
 
-        function prevDefaultCapturingHandler(event) {
-            capturePhases.push(event.currentTarget.className);
-            setCapturingSection();
-        } 
-        function prevDefaultBubblingHandler(event) {
-            event.preventDefault();
-            bubblePhases.push(event.currentTarget.className);
-            setBubblingSection();
-        } 
-        function resetHandler(event) {
-            console.log("resetHandler");
-            capturePhases = [];
-            bubblePhases = [];
-            $('div.capturingSection').empty();
-            $('div.bubblingSection').empty();
-        } 
+    function prevDefaultCapturingHandler(event) {
+        capturePhases.push(event.currentTarget.className);
+        setCapturingSection();
+    }
+    function prevDefaultBubblingHandler(event) {
+        event.preventDefault();
+        bubblePhases.push(event.currentTarget.className);
+        setBubblingSection();
+    }
+    function resetHandler(event) {
+        capturePhases = [];
+        bubblePhases = [];
+        $('div.capturingSection').empty();
+        $('div.bubblingSection').empty();
+    }
 
 });
