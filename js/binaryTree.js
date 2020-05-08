9a6a8ed52fcffe4fe9ba36029e831f63b9f05a1b
SIJU JACOB <SIJUJACOBS@GMAILCOM>
2020-05-08 13:48:32 -0400

    updated tree and node class

--- js/binaryTree.js
+++ js/binaryTree.js
@@ -1,14 +1,44 @@
-function Node(val) {
-  this.value = val;
-  this.left = null;
-  this.right = null;
+function Tree(val) {
+  this.root = null;
+  this.treeMap = {};
 }
 
-Node.prototype.traverse = function (nod, level = 1) {
-  //   console.log("traverse :: nod : ", nod);
-  updateNodeMap(nod, level);
-  let leftNode = nod.left || null;
-  let rightNode = nod.right || null;
+Tree.prototype.updateTreeMap = function (node, level) {
+  let mapObj = {
+    level: level,
+    values: [node.value],
+  };
+  if (this.treeMap[level]) {
+    this.treeMap[level].values.push(mapObj.values[0]);
+  } else {
+    this.treeMap[level] = mapObj;
+  }
+};
+
+Tree.prototype.showAverage = function () {
+  let averages = [];
+  if (this.treeMap) {
+    for (tm in this.treeMap) {
+      let thisLevel = this.treeMap[tm];
+      let total = thisLevel.values.reduce((acc, val) => acc + val);
+      let ave = Math.floor(total / thisLevel.values.length);
+      averages.push(ave);
+    }
+  }
+  return averages;
+};
+Tree.prototype.insertValue = function (n) {
+  var node = new Node(n);
+  if (this.root) {
+    this.root.insertNode(node);
+  } else {
+    this.root = node;
+  }
+};
+Tree.prototype.traverse = function (node = this.root, level = 1) {
+  this.updateTreeMap(node, level);
+  let leftNode = node.left || null;
+  let rightNode = node.right || null;
   if (leftNode || rightNode) {
     ++level;
     if (leftNode) {
@@ -20,61 +50,36 @@ Node.prototype.traverse = function (nod, level = 1) {
   }
 };
 
-Node.prototype.insert = function (val) {
-  if (val < this.value) {
-    if (this.left) {
-      this.left.insert(val);
+function Node(val) {
+  this.value = val;
+  this.left = null;
+  this.right = null;
+}
+
+Node.prototype.insertNode = function (n) {
+  if (n.value < this.value) {
+    if (this.left !== null) {
+      this.left.insertNode(n);
     } else {
-      this.left = new Node(val);
+      this.left = n;
     }
   }
-  if (val > this.value) {
-    if (this.right) {
-      this.right.insert(val);
+  if (n.value > this.value) {
+    if (this.right !== null) {
+      this.right.insertNode(n);
     } else {
-      this.right = new Node(val);
+      this.right = n;
     }
   }
 };
 
-var nodeMap = {};
-function updateNodeMap(node, level) {
-  let nodeDet = {
-    level: level,
-    values: [node.value],
-  };
-  if (nodeMap[level]) {
-    nodeMap[level].values.push(nodeDet.values[0]);
-  } else {
-    nodeMap[level] = nodeDet;
+(function () {
+  var tree = new Tree();
+  for (let i = 1; i < 20; i++) {
+    tree.insertValue(Math.floor(Math.random() * 100 + 1));
   }
-}
-function showAverage() {
-  let averages = [];
-  if (nodeMap) {
-    for (n in nodeMap) {
-      let thisLevel = nodeMap[n];
-      let total = thisLevel.values.reduce((acc, val) => acc + val);
-      let ave = (total / thisLevel.values.length).toFixed(2);
-      //   console.log("ave : ", ave);
-      averages.push(ave);
-    }
-  }
-  return averages;
-}
-
-console.clear();
-var n = new Node(13);
-n.insert(3);
-n.insert(36);
-n.insert(23);
-n.insert(33);
-n.insert(22);
-n.insert(12);
-n.insert(56);
-
-n.traverse(n);
-
-console.log(n);
-console.log("nodeMap : ", nodeMap);
-console.log("showAverage : ", showAverage());
+  console.log("tree : ", tree);
+  tree.traverse();
+  console.log("treeMap : ", tree.treeMap);
+  console.log("showAverage : ", tree.showAverage());
+})();
